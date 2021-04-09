bbd2b972a424917625c9838ee0997b130106740f
kcsar <kcsar@DESKTOP-GRMRODS.lan>
2021-04-08 23:05:17 -0500

    created a servletLogin project, Database Connection, and many more

---
+++ Locust1/src/main/java/com/example/demo/Locust1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Locust1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Locust1Application.class, args);
+	}
+
+}
---
+++ Locust1/src/main/java/com/example/demo/SarojServletLogin.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class SarojServletLogin extends HttpServlet {
+
+		public void doGet (HttpServletRequest req, HttpServletResponse res) {
+			String uid = req.getParameter("username");
+			String pwd = req.getParameter("password");
+			System.out.println(req.getQueryString());
+			
+			try {
+				if(uid.equals("saroj") && pwd.equals("123")) {
+					res.getWriter().write(" login is successful ");
+
+
+
+				 // text format response
+
+				//req.getSession().setAttribute("login", "success");
+				//res.sendRedirect("welcome.html");
+				}
+
+				else {
+					//req.getSession().setAttribute("login", "failed");
+					res.getWriter().write("login failed");
+					
+//					byte[] b="login failed".getBytes();
+//					res.getOutputStream().write(b); // binary format response
+
+//					req.getSession().setAttribute("login", "failed");
+//					req.getRequestDispatcher("error.jsp").forward(req, res);
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+
+		public void doPost() {
+
+		}
+
+		public void init() {
+			System.out.println("System is running for the first time.");
+		}
+
+		public void destroy() {
+			System.out.println("This helps clearing all data.");
+		}
+	}
